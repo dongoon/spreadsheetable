b34eb2ceb3c774dc76bf3053a16fac7e2cd7daa8
Yasuhiko Maeda <y.maeda@dongoon.jp>
2014-04-22 20:55:46 +0900

    circumvent not work coverage config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,18 +1,7 @@
 require 'rspec'
 require 'factory_girl'
-require 'simplecov'
-require 'coveralls'
 require 'spreadsheetable'
 
 # load spec/support/**/*.rb
 Dir["#{File.dirname(__FILE__)}/support/*.rb"].each {|f| require f}
-
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
-  Coveralls::SimpleCov::Formatter,
-  SimpleCov::Formatter::HTMLFormatter,
-]
-
-SimpleCov.start do
-  add_filter "spec/support"
-end if ENV['COVERAGE']
 
